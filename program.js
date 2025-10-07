5b2c78b8af50ea4cab644c7668feb17619317335
DanielGolebowski <126953413+DanielGolebowski@users.noreply.github.com>
2025-10-07 09:23:52 +0200

    Create program.js

---
+++ program.js
@@ -0,0 +1,21 @@
+const dodaj = () => {
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+  const wynik = document.getElementById('wynik')
+
+  const a = parseFloat(f1.value.replace(',','.'))||0;
+  const b = parseFloat(f2.value.replace(',','.'))||0;
+   wynik.innerText = a+b;
+}
+
+const wyczysc = () =>{
+
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+  const wynik = document.getElementById('wynik')
+   wynik.innerText = '';
+  )
+  document.addEvenListner('DOMContentLoaded', () => {
+    document.getElementById ('calcBTN').addEvenListner('click', dodaj);
+    document.getElementById ('clearBTN').addEvenListner('click', wyczyść);
+  }};
