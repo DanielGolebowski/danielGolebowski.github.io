3c23ac4170865a45bc6be2664c893d90833ff8c8
DanielGolebowski <126953413+DanielGolebowski@users.noreply.github.com>
2025-10-08 08:14:31 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,24 +1,24 @@
 const dodaj = () => {
-   const f1 = document.getElementById('f1')
-   const f2 = document.getElementById('f2')
-  const wynik = document.getElementById('wynik')
+  const f1 = document.getElementById('f1');
+  const f2 = document.getElementById('f2');
+  const wynik = document.getElementById('wynik');
 
-  const a = parseFloat(f1.value.replace(',','.'))||0;
-  const b = parseFloat(f2.value.replace(',','.'))||0;
-   wynik.innerText = a+b;
-}
+  const a = parseFloat(f1.value.replace(',', '.')) || 0;
+  const b = parseFloat(f2.value.replace(',', '.')) || 0;
+  wynik.innerText = a + b;
+};
 
-const wyczysc = () =>{
+const wyczysc = () => {
+  const f1 = document.getElementById('f1');
+  const f2 = document.getElementById('f2');
+  const wynik = document.getElementById('wynik');
 
-   const f1 = document.getElementById('f1')
-   const f2 = document.getElementById('f2')
-  const wynik = document.getElementById('wynik')
+  f1.value = '';
+  f2.value = '';
+  wynik.innerText = '';
+};
 
-   f1.value = '';
-   f2.value = '';
-   wynik.innerText = '';
-  )
-  document.addEvenListner('DOMContentLoaded', () => {
-    document.getElementById ('calcBtn').addEvenListner('click', dodaj);
-    document.getElementById ('clearBtn').addEvenListner('click', wyczysc);
-  }};
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('calcBtn').addEventListener('click', dodaj);
+  document.getElementById('clearBtn').addEventListener('click', wyczysc);
+});
